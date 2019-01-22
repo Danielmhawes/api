e251351e08338be85f0da12bdd7469a4bb06919f
Danielmhawes <danielmhawes002@gmail.com>
2019-01-22 12:19:05 +0200

    a task we have to do using api and ajax

---
+++ js/backend.js
@@ -0,0 +1,11 @@
+// request data from back end
+
+// put simply, HTTP IS A PROTOCOL WHICH ALLOWS US TO SEND info arround the world. For example when the browseer asks for http://domain. HTTP requests come in differeent flavors, usuing different verbs (GEET< POST < PUT  and DELETE are the 4 most common) , the kind of request your
+
+
+// Basically , it is aa system that allows us to send requests and handle the response from with un our JAVASCRIPT CODE. FRO EXAMPLE , WHEM YOU LEAVE a comment on a blog post, when you recieve or send on a gmail or email or when a web page updates itself without refreshing , it is often using ajax to do so.
+
+.
+// the two ither initialisms you may see in yhis contexts are API AND JSON
+
+// api stands for application programing interface, many companies and serives provide APIs which will react inspecific ways when we send them HHTTP requst. you will come accross a few in thus course
